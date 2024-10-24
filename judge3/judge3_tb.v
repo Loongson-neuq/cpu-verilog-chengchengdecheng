5c8eb56155b6b86155be8cdd6820be8df7fb426e
Boyao Feng <3061715126@qq.com>
2024-10-24 13:29:45 +0800

    CPUzuoye01

--- judge3/judge3_tb.v
+++ judge3/judge3_tb.v
@@ -14,3 +14,32 @@ testbench激励文件
 $finish;
 
 */
+`timmescale 1ns/1ps
+module judge3_tb;
+    reg a, b, c;
+    wire out;
+        judge UUT (
+            .a(a),
+            .b(b),
+            .c(c),
+            .out(out)
+            );
+    initial begin
+        a=0;b=0;c=0;
+        #10;
+        a=1;b=1;c=1;
+        #10 ;
+        a=0;b=1;c=0;
+        #10;
+        a=0;b=0;c=1;
+        #10;
+        a=0;b=1;c=1;
+        #10;
+        a=1;b=0;c=1;
+        #10;
+        a=1;b=1;c=0;
+        #10;
+        a=1;b=1;c=1;
+        #a0;
+    end        
+endmodule    
--- timer/timer_tb.v
+++ timer/timer_tb.v
@@ -5,3 +5,16 @@ testbench激励文件
 always #5 clk = ~clk;
 
 */
+`timmescale 1ns/1ps
+module timer_tb;
+        reg clk;
+        wire [7:0] timer;
+        timer uut (
+            .clk(clk),
+            .timer(timer)
+        );
+        always #5 clk =~clk;
+    initial begin
+        clk =0;
+    end
+endmodule           
